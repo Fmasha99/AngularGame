886f91e46ef93eee6c8bd37fee62700a02096c90
Fmasha99 <Fmasha99@mail.ru>
2020-08-06 12:32:03 +0300

    Refactor layout.

--- src/app/board/cell/cell.component.ts
+++ src/app/board/cell/cell.component.ts
@@ -1,21 +1,21 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, HostBinding, Input } from "@angular/core";
 import { MainLogicService } from "src/app/main-logic.service";
 import { Cell } from "../board.component";
-
 
 @Component({
 	selector: "app-cell",
 	templateUrl: "./cell.component.html",
-	styleUrls: ["./cell.component.scss"]
+	styleUrls: ["./cell.component.scss"],
 })
 export class CellComponent {
-
 	@Input() public serialNumber: number;
 	@Input() public isActive: boolean;
 	@Input() public x: number;
 	@Input() public y: number;
 
-	// tslint:disable-next-line:no-shadowed-variable
-	public constructor(public mainLogicService: MainLogicService) {
+	public constructor(public mainLogicService: MainLogicService) {}
+
+	@HostBinding("style.backgroundColor") public get backgroundColor() {
+		return this.mainLogicService.changeBackgroundColor(this.x, this.y);
 	}
 }

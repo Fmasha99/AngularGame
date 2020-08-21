ff88afac83112dbbf6c246e1ca97bd39fbb694de
Fmasha99 <Fmasha99@mail.ru>
2020-08-21 18:01:49 +0300

    create array of users

--- src/app/authorize/authorize.component.ts
+++ src/app/authorize/authorize.component.ts
@@ -1,6 +1,11 @@
 import { ChangeDetectorRef, Component, } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
+export interface AuthorizeForm {
+	log: string;
+	pass: string;
+}
+
 @Component({
 	selector: "app-authorize",
 	templateUrl: "./authorize.component.html",
@@ -8,7 +13,13 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class AuthorizeComponent {
 	public playerForm: FormGroup;
-	public arrayOfPlayers: [];
+	public arrayOfUsers: AuthorizeForm[] = [
+		{ log: "Fmasha", pass: "Fmasha99" },
+		{ log: "AlinaD", pass: "CDf02dnljA" },
+		{ log: "KirKorol", pass: "Kamc122FF" },
+		{ log: "SkibDasha", pass: "PG32ooHsmv" },
+		{ log: "HalinaFil", pass: "gitClone55" }
+	];
 
 	public constructor(
 		public formBuilder: FormBuilder,
@@ -16,14 +27,14 @@ export class AuthorizeComponent {
 		this.playerForm = this.formBuilder.group({
 			login: [null, [
 				Validators.required,
-				Validators.minLength(6),
+				Validators.minLength(5),
 				Validators.pattern("^[a-zA-Z]+$")]
 			],
 			password: [null, [
 				Validators.required,
 				Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/),
 				Validators.minLength(4),
-				Validators.maxLength(8)]
+				Validators.maxLength(12)]
 			]
 		})
 
